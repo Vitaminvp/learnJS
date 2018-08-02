b7a7f7ff55808764466072b3c9bfa69d35382683
Vitamin <vitamin@ukr.net>
2018-08-02 15:47:38 +0300

    next

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+function HelloWorld() {
+    return <h1>Hello World</h1>
+}
+
+render(<HelloWorld/>, document.querySelector('#container'));
