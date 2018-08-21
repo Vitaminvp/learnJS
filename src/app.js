1e5bdecc3030b5dedf9ada8bc035f1ceb5b6fa3a
Vitamin <vitamin@ukr.net>
2018-08-21 17:53:34 +0300

    m

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 import * as React from 'react';
 import ArticleList from './js/components/articleList';
 import './js/components/article.css';
-import {UserForm} from "./js/components/userForm";
+import {UserForm} from './js/components/userForm';
 import {Filters} from './js/filters';
-import Counter from "./js/components/counter";
+import Counter from './js/components/counter';
 
 
 export default class App extends React.Component{
-    render(){
-        return <div>
-            <Counter/>
-            <UserForm/>
-            <Filters articles = {[]} />
-            <ArticleList/>
-        </div>
-    }
+	render(){
+		return <div>
+                <Counter/>
+                <UserForm/>
+                <Filters articles = {[]} />
+                <ArticleList/>
+		    </div>;
+	}
 }
